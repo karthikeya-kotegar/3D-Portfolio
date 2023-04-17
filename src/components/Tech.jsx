60c1a83251bff022dad634b7cd01042f55d33da3
karthikeya-kotegar <karthikeyakotegar@gmail.com>
2023-04-17 19:56:19 +0530

    tech and projects

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+import { BallCanvas } from "./canvas";
+import { technologies } from "../constants";
+import { SectionWrapper } from "../hoc";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I am good at</p>
+        <h2 className={styles.sectionHeadText}>Technologies</h2>
+      </motion.div>
 
-export default Tech
+      <div className="flex flex-row flex-wrap justify-center gap-10 mt-8">
+        {technologies.map((tech, index) => {
+          return (
+            <div className="w-28 h-28" key={tech.name}>
+              <BallCanvas icon={tech.icon} />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Tech, "");
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,15 @@ module.exports = {
       backgroundImage: {
         "hero-pattern": "url('/src/assets/herobg.png')",
       },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
